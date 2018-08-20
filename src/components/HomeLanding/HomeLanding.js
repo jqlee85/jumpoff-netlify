1ef57ef1a4ee9236763dce8c1167b2d5f4b3292a
Jesse Lee <jqlee85@gmail.com>
2018-08-20 17:11:41 -0300

    test height

--- src/components/HomeLanding/HomeLanding.js
+++ src/components/HomeLanding/HomeLanding.js
@@ -20,6 +20,7 @@ class HomeLanding extends Component {
   viewportHeight() {
     let vh = window.innerHeight * 0.01;
     document.documentElement.style.setProperty('--vh', `${vh}px`);
+    alert('window.innerHeight =' + window.innerHeight);
   }
 
   render(){
