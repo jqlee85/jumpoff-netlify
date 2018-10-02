bed5551662fdfeeb9652ea1b8eed269d2183a8cf
Jesse Lee <jqlee85@gmail.com>
2018-10-01 21:08:52 -0300

    add lorem ipsum

--- src/components/AccordionSection/AccordionSection.js
+++ src/components/AccordionSection/AccordionSection.js
@@ -33,7 +33,7 @@ function AccordionSection(props) {
         </ExpansionPanelSummary>
         <ExpansionPanelDetails className="accordion-content">
           <p>
-            Lorem Ipsum sit dolor
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
           </p>
           <a><button className="accordion-button black">Learn More</button></a>
         </ExpansionPanelDetails>
