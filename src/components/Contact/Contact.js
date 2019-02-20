df8c7eed16875c02cbdd2775a22ec9e951297de4
Jesse Lee <jqlee85@gmail.com>
2019-02-19 21:52:23 -0300

    add state to contact page

--- src/components/Contact/Contact.js
+++ src/components/Contact/Contact.js
@@ -4,21 +4,44 @@ import styles from './Contact.css';
 
 class Contact extends Component {
   
-  render() {
+  constructor(props){
+    super(props);
+    this.state = { 
+      name: '',
+      email: '',
+      message: '',
+    };  
+  }
 
+  handleSubmit = e => {
+    fetch("/", {
+      method: "POST",
+      headers: { "Content-Type": "application/x-www-form-urlencoded" },
+      body: { "form-name": "contact", ...this.state }
+    })
+      .then(() => alert("Success!"))
+      .catch(error => alert(error));
+
+    e.preventDefault();
+  };
+
+  handleChange = e => this.setState({ [e.target.name]: e.target.value });
+
+  render() {
+    const { name, email, message } = this.state;
     return <section className="contact-page">
       <div className="jo-row">
         <div className="jo-content">
-          <form name="contactpageform" method="post">
+          <form name="contactpageform" method="post" onSubmit={this.handleSubmit}>
             <input type="hidden" name="form-name" value="contactpageform" />
             <p>
-              <label>Your Name: <input type="text" name="name"/></label>
+              <label>Your Name: <input type="text" name="name" value={this.state.name} onChange={this.handleChange}/></label>
             </p>
             <p>
-              <label>Your Email: <input type="email" name="email"/></label>
+              <label>Your Email: <input type="email" name="email" value={this.state.email} onChange={this.handleChange}/></label>
             </p>
             <p>
-              <label>Message: <textarea name="message"></textarea></label>
+              <label>Message: <textarea name="message" onChange={this.handleChange}>{this.state.message}</textarea></label>
             </p>
             <p>
               <button type="submit">Send</button>
