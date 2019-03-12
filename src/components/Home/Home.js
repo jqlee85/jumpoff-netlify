8882448f61d030ba6f8b8662326317209064b0ee
Jesse Lee <jqlee85@gmail.com>
2019-03-12 10:47:56 -0300

    asdf
    a

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -4,8 +4,6 @@ import HomeSectionTwo from '../HomeSectionTwo/HomeSectionTwo';
 import HomeSectionThree from '../HomeSectionThree/HomeSectionThree';
 import HomeSectionFour from '../HomeSectionFour/HomeSectionFour';
 import HomeSectionFive from '../HomeSectionFive/HomeSectionFive';
-import { Helmet } from 'react-helmet';
-import metaImage from '../../public/images/jumpoff-what-we-do-bg-colored-shapes-multi.jpg';
 
 import  './Home.css';
 
@@ -19,10 +17,6 @@ class Home extends Component {
   
   render(){
     return <div id="home" className="home">      
-      <Helmet>
-        <meta name="image" content={metaImage} />
-        <meta property="og:image" content={metaImage} />
-      </Helmet>
       <HomeLanding/>
       <HomeSectionTwo/>
       <HomeSectionThree/>
