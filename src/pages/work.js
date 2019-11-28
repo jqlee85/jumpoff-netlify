72da0384dbb2a02f4568c36bdcd4aef478f2e0dc
Jesse Lee <jqlee85@gmail.com>
2019-11-27 21:56:58 -0600

    pages

---
+++ src/pages/work.js
@@ -0,0 +1,15 @@
+import React from "react"
+import Layout from "../components/layout"
+import SEO from "../components/seo"
+
+const Work = () => {
+
+    return <Layout>
+        <SEO title="JumpOff, LLC Portfolio" />
+        <article className="jo-content jo-post-content-wrapper">
+            <h1>Work</h1>  
+       </article>
+    </Layout>
+}
+
+export default Work
