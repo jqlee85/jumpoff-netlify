0a2f305bbcce3c7706528100e9014d53a5d1d69b
Jesse Lee <jqlee85@gmail.com>
2019-11-27 22:40:38 -0600

    adding apollo

--- src/pages/blog.js
+++ src/pages/blog.js
@@ -1,22 +1,172 @@
 import React from 'react'
 import {graphql} from 'gatsby'
-import Layout from "../components/layout"
-import SEO from "../components/seo"
+import Layout from '../components/layout'
+import SEO from '../components/seo'
+import './Blog.css';
+import LinkButton from '../LinkButton/LinkButton'
+import ListPosts from '../ListPosts/ListPosts'
+import LoadingShape from '../LoadingShape/LoadingShape'
+import { useQuery } from '@apollo/react-hooks'
+import gql from 'graphql-tag'
 
 const blog = (data) => {
+    
+    let variables = {
+        first: 5,
+        after: ''
+    }
+    
     return <Layout>
         <SEO title="Page two" />
-        <h1>Hi from the second page</h1>
-        <p>Welcome to page 2</p>
-        <Link to="/">Go back to the homepage</Link>
+        <section className="blog latest-posts">
+        <div className="jo-row">
+            <div className="jo-content">
+            <h1 className="standard-title">Latest Posts</h1>
+            <Query query={LATEST_POSTS_QUERY} variables={variables} fetchPolicy="cache-and-network">
+                {({ loading, error, data, fetchMore }) => {
+                if (loading && !data.posts) return (<div className="large-loader-wrapper"><LoadingShape/></div>);
+                if (error) return (<p>Error Loading Posts</p>);
+                if (data.posts) return(
+                    <div>
+                    <ListPosts data={data.posts.edges || []}/>
+                    {data.posts.pageInfo.hasNextPage && 
+                        <div className="jo-more-posts-wrapper">
+                        {loading && <div className="large-loader-wrapper"><LoadingShape/></div>}
+                        {!loading && <LinkButton 
+                            transparent={true} 
+                            linkType="custom" 
+                            text="More Posts" 
+                            onClick={() =>
+                            fetchMore({
+                                query: LATEST_POSTS_QUERY,
+                                variables: {
+                                first: 5,
+                                after: data.posts.pageInfo.endCursor
+                                },
+                                updateQuery: (prev, { fetchMoreResult }) => {
+                                fetchMoreResult.posts.edges.unshift(...prev.posts.edges);
+                                if (!fetchMoreResult) return prev;
+                                return fetchMoreResult
+                                }
+                            })
+                            }
+                        />}
+                        </div>
+                    }
+                    </div>
+                )
+                }}
+            </Query>
+            </div>
+        </div>
+        </section>
     </Layout>
 }
 
 export const personQuery = graphql`
-	query blogQuery {
-        
+    query blogQuery($first: Int!, $after: String!) {
+        posts(
+            first: $first,
+            after: $after,
+            where: {categoryId: 188}
+        ) {
+            pageInfo {
+                hasNextPage
+                endCursor
+            }
+            edges {
+                node {
+                    id
+                    title
+                    slug
+                    date
+                    excerpt
+                    featuredImage {
+                        sourceUrl
+                    }
+                }
+            }
+        }
     }
 `
 
-export default blog
+const LATEST_POSTS_QUERY = gql`
+  query listView($first: Int!, $after: String!) {
+    posts(first: $first, after: $after where: {categoryId:188}) {
+      pageInfo {
+        hasNextPage
+        endCursor
+      }
+      edges {
+        node {
+          id
+          title
+          slug
+          date
+          excerpt
+          featuredImage {
+            sourceUrl
+          }
+        }
+      }
+    }
+  }
+`;
+
+class Blog extends Component {
+
+  render(){
+    
+    let variables = {
+      first: 5,
+      after: ''
+    }
+
+    return (
+    <section className="blog latest-posts">
+      <div className="jo-row">
+        <div className="jo-content">
+          <h1 className="standard-title">Latest Posts</h1>
+          <Query query={LATEST_POSTS_QUERY} variables={variables} fetchPolicy="cache-and-network">
+            {({ loading, error, data, fetchMore }) => {
+              if (loading && !data.posts) return (<div className="large-loader-wrapper"><LoadingShape/></div>);
+              if (error) return (<p>Error Loading Posts</p>);
+              if (data.posts) return(
+                <div>
+                  <ListPosts data={data.posts.edges || []}/>
+                  {data.posts.pageInfo.hasNextPage && 
+                    <div className="jo-more-posts-wrapper">
+                      {loading && <div className="large-loader-wrapper"><LoadingShape/></div>}
+                      {!loading && <LinkButton 
+                        transparent={true} 
+                        linkType="custom" 
+                        text="More Posts" 
+                        onClick={() =>
+                          fetchMore({
+                            query: LATEST_POSTS_QUERY,
+                            variables: {
+                              first: 5,
+                              after: data.posts.pageInfo.endCursor
+                            },
+                            updateQuery: (prev, { fetchMoreResult }) => {
+                              fetchMoreResult.posts.edges.unshift(...prev.posts.edges);
+                              if (!fetchMoreResult) return prev;
+                              return fetchMoreResult
+                            }
+                          })
+                        }
+                      />}
+                    </div>
+                  }
+                </div>
+              )
+            }}
+          </Query>
+        </div>
+      </div>
+    </section>);
+  }
+}
+
+export default Blog;
 
