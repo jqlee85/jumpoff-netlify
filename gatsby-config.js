ae324e7fad462c338e4dbe647c5f13b5d308dcdf
Jesse Lee <jqlee85@gmail.com>
2019-12-26 13:35:36 -0600

    add gatsby styled components

--- gatsby-config.js
+++ gatsby-config.js
@@ -51,6 +51,12 @@ module.exports = {
         ],
       },
     },
+    {
+      resolve: `gatsby-plugin-styled-components`,
+      options: {
+        // Add any options here
+      },
+    },
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
     // `gatsby-plugin-offline`,
